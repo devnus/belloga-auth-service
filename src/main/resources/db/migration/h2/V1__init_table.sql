b3715de4a99626bf706167b2633fa57529d3d1bb
genius-jo <joks7285@daum.net>
2022-07-17 03:58:44 +0900

    feat: add fly way for init table

---
+++ src/main/resources/db/migration/h2/V1__init_table.sql
@@ -0,0 +1,15 @@
+/* 소셜로그인과 커스텀로그인 공통 계정 정보를 저장할 테이블 */
+CREATE TABLE account (
+    account_id VARCHAR(255) NOT NULL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    is_locked BOOLEAN NOT NULL,
+    account_role VARCHAR(255) Not NULL,
+    dtype VARCHAR(255) NOT NULL
+);
+
+/* 커스텀로그인 계정 정보를 저장할 테이블 */
+CREATE TABLE custom (
+    account_id VARCHAR(255) NOT NULL PRIMARY KEY,
+    password VARCHAR(255) NOT NULL,
+    FOREIGN KEY (account_id) REFERENCES account (account_id)
+);
