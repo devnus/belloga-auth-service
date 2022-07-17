9f8bdc723dfd2b81ba9013df537ecd0ef0006548
genius-jo <joks7285@daum.net>
2022-07-17 23:28:03 +0900

    refactor : move encryption method to common

--- src/main/java/com/devnus/belloga/auth/account/service/AccountService.java
+++ src/main/java/com/devnus/belloga/auth/account/service/AccountService.java
@@ -1,12 +1,8 @@
 package com.devnus.belloga.auth.account.service;
 
-import com.devnus.belloga.auth.account.domain.Account;
 import com.devnus.belloga.auth.account.dto.RequestAccount;
 import com.devnus.belloga.auth.account.dto.ResponseAccount;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface AccountService {
     ResponseAccount.RegisterAccount saveCustomAccountEnterprise(RequestAccount.RegisterCustomAccountEnterprise request);
-    String encrypt(String text);
 }
