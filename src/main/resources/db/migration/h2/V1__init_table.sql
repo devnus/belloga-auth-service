2e780a322113815d60a9efd4d22b5b614c32928c
genius-jo <joks7285@daum.net>
2022-07-21 22:22:53 +0900

    refactor: remove unneeded sql

--- src/main/resources/db/migration/h2/V1__init_table.sql
+++ src/main/resources/db/migration/h2/V1__init_table.sql
@@ -3,7 +3,6 @@ CREATE TABLE account (
     account_id VARCHAR(255) NOT NULL PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     is_locked BOOLEAN NOT NULL,
-    account_role VARCHAR(255) Not NULL,
     dtype VARCHAR(255) NOT NULL
 );
 
