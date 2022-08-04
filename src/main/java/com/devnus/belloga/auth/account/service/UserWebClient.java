0834a6f187ed7128486c5697de4e56bb21eca1c1
genius-jo <joks7285@daum.net>
2022-08-05 02:37:29 +0900

    fix : Fix the deserialized response value into LinkedHashMap to convert it to ResponseUser.UserInfo type

--- src/main/java/com/devnus/belloga/auth/account/service/UserWebClient.java
+++ src/main/java/com/devnus/belloga/auth/account/service/UserWebClient.java
@@ -1,8 +1,9 @@
 package com.devnus.belloga.auth.account.service;
 
 import com.devnus.belloga.auth.account.dto.ResponseUser;
-import com.devnus.belloga.auth.common.aop.annotation.UserRole;
 import com.devnus.belloga.auth.common.dto.CommonResponse;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 @RequiredArgsConstructor
 public class UserWebClient {
     private final WebClient webClient;
+    private final ObjectMapper objectMapper;
 
     /**
      * 동기 통신을 통해 User마이크로서비스에서 accountId를 이용해 해당하는 UserId와 UserRole을 가져온다
@@ -26,6 +28,7 @@ public class UserWebClient {
                 .bodyToMono(CommonResponse.class)
                 .block();
 
-        return (ResponseUser.UserInfo) commonResponse.getResponse();
+        //LinkedHashMap으로 역직렬화 된 commonResponse의 response 값을 ResponseUser.UserInfo타입으로 형변환
+        return objectMapper.convertValue(commonResponse.getResponse(), new TypeReference<ResponseUser.UserInfo>() {});
     }
 }
