350a97ca41aa68c7946222066a4f418fecb78738
genius-jo <joks7285@daum.net>
2022-10-04 17:52:24 +0900

    feat: add application prod setting

---
+++ src/main/resources/db/migration/mysql/V1__init_table.sql
@@ -0,0 +1,24 @@
+/* 소셜로그인과 커스텀로그인 공통 계정 정보를 저장할 테이블 */
+CREATE TABLE account (
+    account_id VARCHAR(255) NOT NULL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    is_locked BOOLEAN NOT NULL,
+    user_role VARCHAR(255) NOT NULL,
+    created_date TIMESTAMP,
+    last_modified_date TIMESTAMP,
+    dtype VARCHAR(255) NOT NULL
+);
+
+/* 커스텀로그인 계정 정보를 저장할 테이블 */
+CREATE TABLE custom (
+    account_id VARCHAR(255) NOT NULL PRIMARY KEY,
+    password VARCHAR(255) NOT NULL,
+    FOREIGN KEY (account_id) REFERENCES account (account_id)
+);
+
+/* oauth 계정 정보를 저장할 테이블 */
+CREATE TABLE oauth (
+    account_id VARCHAR(255) NOT NULL PRIMARY KEY,
+    auth_provider VARCHAR(255) NOT NULL,
+    FOREIGN KEY (account_id) REFERENCES account (account_id)
+);
